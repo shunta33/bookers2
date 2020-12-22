526544d7502cde972b07dd6fc4b55fead71134b2
shunta33 <syuntaochiai33@gmail.com>
2020-12-22 14:33:52 +0000

    [Add]プロフィール編集機能

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!
+
   def index
-    
   end
 
   def show
   end
 
   def edit
+    @user = current_user
   end
+
 end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   resources :books, only: [:new, :create, :edit, :index, :show, :update, :destroy]
   resources :users, only: [:index, :show, :edit]
+  patch 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
