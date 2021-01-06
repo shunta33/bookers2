c746016e23d7615806e05af44f15bbd37af16add
shunta33 <syuntaochiai33@gmail.com>
2021-01-06 12:48:54 +0000

    [Fix]マイグレーションファイルの修正

--- bookers2/db/migrate/20201221114959_devise_create_users.rb
+++ bookers2/db/migrate/20201221114959_devise_create_users.rb
@@ -33,6 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :name
+      t.text :introduction
       t.integer :profile_image_id
       t.timestamps null: false
     end
